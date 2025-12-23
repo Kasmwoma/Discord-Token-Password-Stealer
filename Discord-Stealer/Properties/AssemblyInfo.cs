4557ac4f75b35729879c5ab016fd8a3ab67367b6
CabinCr3w <34693770+CabinCr3ws@users.noreply.github.com>
2025-12-23 20:02:50 +0400

    AssemblyInfo.cs

---
+++ Discord-Stealer/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("0.47.987.1430")]
+[assembly: AssemblyTitle("Discord")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Discord")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("C15AC81E-89C6-4308-8E95-FDA7DC867BEC")]
+[assembly: AssemblyFileVersion("0.47.987.1430")]
